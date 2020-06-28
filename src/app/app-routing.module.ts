1cb127af88f6354994048bd5e103580bafaddb3f
Ali Turab Abbasi <Ali-Turab.Abbasi@t-systems.com>
2020-06-28 04:17:36 +0200

    implemented data table

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {MainComponent} from './main/main.component';
+import { MainComponent } from './main/main.component';
 
 
 const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'main',
+    pathMatch: 'full'
+  },
   {
     path: 'main',
     component: MainComponent
--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, ViewChild, OnInit } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { merge, Observable, of as observableOf } from 'rxjs';
+import { catchError, map, startWith, switchMap } from 'rxjs/operators';
+import { ProxyService } from '../proxy.service';
+import { Proxy } from './proxy';
+import { MatTableDataSource } from '@angular/material/table';
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.css']
 })
 export class MainComponent implements OnInit {
+  displayedColumns: string[] = ['ip', 'port', 'provider', 'basicTest', 'testDate', 'firstfound', 'lastfound'];
 
-  constructor() { }
+  data: Proxy[] | null;
+  dataSource;
+  isLoadingResults = true;
+
+
+
+  constructor(private proxyService: ProxyService) { }
+
+  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
 
   ngOnInit(): void {
-  }
 
+
+    this.proxyService.getProxies().subscribe(res => {
+      // this.resultsLength = res.length;
+      this.dataSource = new MatTableDataSource(res);
+      console.log('this is dta', this.data);
+      this.dataSource.paginator = this.paginator;
+      this.isLoadingResults = false;
+    });
+  }
 }
+
